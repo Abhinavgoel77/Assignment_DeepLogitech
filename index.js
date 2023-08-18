17eb0311598a513aab02cf4cc0cd48aaeebac342
Abhinav goel <Abhinavgoel77@gmail.com>
2023-08-18 13:57:11 +0530

    extraction method updated

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const server = http.createServer((req, res) => {
       response.on('end', () => {
         const latestStories = extractLatestStories(data);
         res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify(latestStories, null, 2));
+        res.end(JSON.stringify(latestStories));
       });
     }).on('error', (error) => {
       res.writeHead(500, { 'Content-Type': 'text/plain' });
@@ -32,15 +32,37 @@ server.listen(3000, () => {
 });
 
 function extractLatestStories(html) {
-  const stories = [];
-  let match;
-  const storyRegex = /<li class="latest-stories__item">.*?<a href="(.*?)">\s*<h3 class="latest-stories__item-headline">(.*?)<\/h3>/gs;
+  const topStories = [];
 
-  while ((match = storyRegex.exec(html)) !== null && stories.length < 6) {
-    const link = match[1];
-    const title = match[2];
-    stories.push({ title, link });
-  }
+const startMarker = "<h2 class=\"latest-stories__heading\">Latest Stories</h2>\n";
+const endMarker = "</time>\n            </li>\n          </ul>";
 
-  return stories;
+const startIndex = html.indexOf(startMarker);
+
+const endIndex = html.indexOf(endMarker, startIndex + startMarker.length);
+
+const extractedContent = html.substring(startIndex + startMarker.length, endIndex);
+// console.log("the content is",extractedContent);
+
+const items = extractedContent.split('<li class="latest-stories__item">');
+// console.log("items are",items);
+
+items.shift();
+
+for (let i = 0; i < 6; i++) {
+  const item = items[i];
+
+  // Extract title
+  const titleStartIndex = item.indexOf('<h3 class="latest-stories__item-headline">');
+  const titleEndIndex = item.indexOf('</h3>', titleStartIndex);
+  const title = item.substring(titleStartIndex + '<h3 class="latest-stories__item-headline">'.length, titleEndIndex).trim();
+
+  // Extract link
+  const linkStartIndex = item.indexOf('<a href="');
+  const linkEndIndex = item.indexOf('">', linkStartIndex);
+  const link = item.substring(linkStartIndex + '<a href="'.length, linkEndIndex);
+
+  topStories.push({ title, link });
+}
+  return topStories;
 }
